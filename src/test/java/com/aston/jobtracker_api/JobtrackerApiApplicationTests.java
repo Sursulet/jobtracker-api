d3745edaec5cbc73abfa9cdc10809edc6b4eefac
sursu <stursulet@gmail.com>
2025-07-29 11:39:36 +0200

    initial commit : Spring Boot JobTracker API

---
+++ src/test/java/com/aston/jobtracker_api/JobtrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aston.jobtracker_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobtrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
