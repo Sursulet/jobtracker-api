92437e3797ecc3a34d72e313d4c7c15c3f5c3317
sursu <stursulet@gmail.com>
2025-09-04 14:50:10 +0200

    first commit

---
+++ src/test/java/com/example/jobtracker_api/JobtrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jobtracker_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobtrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
